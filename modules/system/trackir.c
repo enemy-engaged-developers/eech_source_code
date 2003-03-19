37ac244481bccecb6a57fa25f53076b1e8fe97fe
Retro <Retro>
2003-03-19 20:09:31 +0000

    TrackIR-support STUB file

---
+++ modules/system/trackir.c
@@ -0,0 +1,38 @@
+//**********************************************************************
+//	Filename:	TrackIR.c
+//	Authors:	Lukas "Retro" Friembichler (adapted for EECH)
+//	Date:		18. Mar 2003
+//	Update:
+//
+//	Description:	THIS IS JUST A STUB FILE ! (due to license issues)
+//					To get (enhanced) TIR functionality working, contact
+//
+//			Retro
+//					through eech-dev-forum @ www.biohazcentral.com
+//					(at least till we work out a final solution)
+//*********************************************************************/
+
+int query_TIR_active ()
+{
+	return 0;
+}
+
+int GetYaw()
+{
+	return 0;
+}
+
+int GetPitch()
+{
+	return 0;
+}
+
+void InitTrackIR()
+{
+
+}
+
+void ExitTrackIR()
+{
+
+}
