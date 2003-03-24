78c18b7744698da00d847b4e6bde3dd20cc5cf4e
crh <crh>
2003-03-24 13:06:49 +0000

    added quick hack header file for trackir functions - need function prototypes for Open Watcom build

---
+++ modules/system/trackir.h
@@ -0,0 +1,13 @@
+/* CRH 030320 */
+/* quick hack to get compile working with OpenWatcom */
+
+extern int query_TIR_active ( void );
+
+extern int GetYaw ( void );
+
+extern int GetPitch ( void );
+
+extern void InitTrackIR ( void );
+
+extern void ExitTrackIR ( void );
+
