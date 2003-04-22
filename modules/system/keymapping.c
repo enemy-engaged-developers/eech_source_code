0e4a8a33a15b12eb42a192d86fcb62bda2174185
Retro <Retro>
2003-04-22 18:27:00 +0000

    Mainly involves getting rid of all the 'if's

--- modules/system/keymapping.c
+++ modules/system/keymapping.c
@@ -10,21 +10,180 @@
 
 #include "cmndline.h"
 
-#define SHOW_UNKNOWNS	// if there´s a command registerd (by using set_configurable_event () that is NOT in
+//#define SHOW_UNKNOWNS	// if there´s a command registerd (by using set_configurable_event () that is NOT in
 						// the keymaps.cfg file, I get an error-output file (hopefully)
+//#define TIGHT_ARSE_DEBUG
 
 #define MAXMAPPINGS 300		// Max number of mappings, no real reason for 300 (just a number)
 
-#define SLIZE 50	// max length one command, keycombo or modifier can have.. (I´m wary of malloc())
+#define SLIZE 50	// max length one command, keycombo or modifier can have.. (I´m still unconfortable using malloc())
+
+//**********************************************************************
+//	this struct represents one key;
+//	the name as it is represented in the config file, and the assigned
+//	direct-input-keypress (DIK)
+//*********************************************************************/
+typedef struct {
+	char* key_name;
+	int dik_val;
+} key;
+
+#define NUM_OF_KEYS 142
+
+	key all_keys[NUM_OF_KEYS] = {
+
+	{"ESCAPE",		0x01},
+	{"1",			0x02},
+	{"2",			0x03},
+	{"3",			0x04},
+	{"4",			0x05},
+	{"5",			0x06},
+	{"6",			0x07},
+	{"7",			0x08},
+	{"8",			0x09},
+	{"9",			0x0A},
+	{"0",			0x0B},
+	{"MINUS",		0x0C},	  /* - on main keyboard */
+	{"EQUALS",		0x0D},
+	{"BACK",		0x0E},    /* backspace */
+	{"BACKSPACE",	0x0E},    /* backspace */
+	{"TAB",			0x0F},
+	{"Q",			0x10},
+	{"W",			0x11},
+	{"E",			0x12},
+	{"R",			0x13},
+	{"T",			0x14},
+	{"Y",			0x15},
+	{"U",			0x16},
+	{"I",			0x17},
+	{"O",			0x18},
+	{"P",			0x19},
+	{"LBRACKET",	0x1A},
+	{"RBRACKET",	0x1B},
+	{"RETURN",		0x1C},    /* Enter on main keyboard */
+	{"LCONTROL",	0x1D},
+	{"A",			0x1E},
+	{"S",			0x1F},
+	{"D",			0x20},
+	{"F",			0x21},
+	{"G",			0x22},
+	{"H",			0x23},
+	{"J",			0x24},
+	{"K",			0x25},
+	{"L",			0x26},
+	{"SEMICOLON",	0x27},
+	{"APOSTROPHE",	0x28},
+	{"GRAVE",		0x29},    /* accent grave */
+	{"LSHIFT",		0x2A},
+	{"BACKSLASH",	0x2B},
+	{"Z",			0x2C},
+	{"X",			0x2D},
+	{"C",			0x2E},
+	{"V",			0x2F},
+	{"B",			0x30},
+	{"N",			0x31},
+	{"M",			0x32},
+	{"COMMA",		0x33},
+	{"PERIOD",		0x34},    /* . on main keyboard */
+	{"SLASH",		0x35},    /* / on main keyboard */
+	{"RSHIFT",		0x36},
+	{"MULTIPLY",	0x37},
+	{"NUMPADSTAR",	0x37},    /* * on numeric keypad */
+	{"LMENU",		0x38},
+	{"LALT",		0x38},    /* left Alt */
+	{"SPACE",		0x39},
+	{"CAPITAL",		0x3A},
+	{"CAPSLOCK",	0x3A},
+	{"F1",			0x3B},
+	{"F2",			0x3C},
+	{"F3",			0x3D},
+	{"F4",			0x3E},
+	{"F5",			0x3F},
+	{"F6",			0x40},
+	{"F7",			0x41},
+	{"F8",			0x42},
+	{"F9",			0x43},
+	{"F10",			0x44},
+	{"NUMLOCK",		0x45},
+	{"SCROLL",		0x46},    /* Scroll Lock */
+	{"NUMPAD7",		0x47},
+	{"NUMPAD8",		0x48},
+	{"NUMPAD9",		0x49},
+	{"SUBTRACT",	0x4A},
+	{"NUMPADMINUS",	0x4A},    /* - on numeric keypad */
+	{"NUMPAD4",		0x4B},
+	{"NUMPAD5",		0x4C},
+	{"NUMPAD6",		0x4D},
+	{"ADD",			0x4E},
+	{"NUMPADPLUS",	0x4E},    /* + on numeric keypad */
+	{"NUMPAD1",		0x4F},
+	{"NUMPAD2",		0x50},
+	{"NUMPAD3",		0x51},
+	{"NUMPAD0",		0x52},
+	{"DECIMAL",		0x53},
+	{"NUMPADPERIOD",0x53},    /* . on numeric keypad */
+	{"OEM_102",		0x56},    /* < > | on UK/Germany keyboards */
+	{"F11",			0x57},
+	{"F12",			0x58},
+	{"F13",			0x64},    /*                     (NEC PC98) */
+	{"F14",			0x65},    /*                     (NEC PC98) */
+	{"F15",			0x66},    /*                     (NEC PC98) */
+	{"KANA",		0x70},    /* (Japanese keyboard)            */
+	{"ABNT_C1",		0x73},    /* / ? on Portugese (Brazilian) keyboards */
+	{"CONVERT",		0x79},    /* (Japanese keyboard)            */
+	{"NOCONVERT",	0x7B},    /* (Japanese keyboard)            */
+	{"YEN",			0x7D},    /* (Japanese keyboard)            */
+	{"ABNT_C2",		0x7E},    /* Numpad . on Portugese (Brazilian) keyboards */
+	{"NUMPADEQUALS",0x8D},    /* = on numeric keypad (NEC PC98) */
+	{"CIRCUMFLEX",	0x90},    /* (Japanese keyboard)            */
+	{"AT",			0x91},    /*                     (NEC PC98) */
+	{"COLON",		0x92},    /*                     (NEC PC98) */
+	{"UNDERLINE",	0x93},    /*                     (NEC PC98) */
+	{"KANJI",		0x94},    /* (Japanese keyboard)            */
+	{"STOP",		0x95},    /*                     (NEC PC98) */
+	{"AX",			0x96},    /*                     (Japan AX) */
+	{"UNLABELED",	0x97},    /*                        (J3100) */
+	{"NUMPADENTER",	0x9C},    /* Enter on numeric keypad */
+	{"RCONTROL",	0x9D},
+	{"NUMPADCOMMA",	0xB3},    /* , on numeric keypad (NEC PC98) */
+	{"DIVIDE",		0xB5},
+	{"NUMPADSLASH",	0xB5},    /* / on numeric keypad */
+	{"SYSRQ",		0xB7},
+	{"RMENU",		0xB8},
+	{"RALT",		0xB8},    /* right Alt */
+	{"PAUSE",		0xC5},    /* Pause */
+	{"HOME",		0xC7},    /* Home on arrow keypad */
+	{"UP",			0xC8},
+	{"UPARROW",		0xC8},
+	{"PRIOR",		0xC9},
+	{"PGUP",		0xC9},   /* PgUp on arrow keypad */
+	{"LEFT",		0xCB},
+	{"LEFTARROW",	0xCB},    /* LeftArrow on arrow keypad */
+	{"RIGHT",		0xCD},
+	{"RIGHTARROW",	0xCD},    /* RightArrow on arrow keypad */
+	{"END",			0xCF},    /* End on arrow keypad */
+	{"DOWN",		0xD0},
+	{"DOWNARROW",	0xD0},    /* DownArrow on arrow keypad */
+	{"NEXT",		0xD1},
+	{"PGDN",		0xD1},    /* PgDn on arrow keypad */
+	{"INSERT",		0xD2},    /* Insert on arrow keypad */
+	{"DELETE",		0xD3},    /* Delete on arrow keypad */
+	{"LWIN",		0xDB},    /* Left Windows key */
+	{"RWIN",		0xDC},    /* Right Windows key */
+	{"APPS",		0xDD},    /* AppMenu key */
+	{"POWER",		0xDE},
+	{"SLEEP",		0xDF},
+	{"WAKE",		0xE3}
+	};
 
 //**********************************************************************
 //	this struct represents one line of the config-file;
 //	functionID, key, modifer and state
 //*********************************************************************/
-typedef struct keymap {
+typedef struct {
 	char	functionID[SLIZE];
-	int		key,
-			modifier,
+	int		key,		// This is actually the DIK of the key..
+			modifier,	// I guess with multiple modifiers per key this could become a bitfield..
 			state;
 } mapline;
 
@@ -45,6 +204,30 @@ static int number_of_commands = 0;	// number of keymappings in the above array
 //*********************************************************************/
 int check_for_validity ( void )
 {
+	int i = 0;
+	int j = 0;
+	int numOfDuplicates = 0;
+
+	int tmp_key;
+	int tmp_mod;
+
+	for ( i = 0; i < number_of_commands; i++)
+	{
+		tmp_key = keymaps[i].key;
+		tmp_mod = keymaps[i].modifier;
+
+		for ( j = 0; j < number_of_commands; j++)
+		{
+			if ((keymaps[i].key = tmp_key)&&(keymaps[i].modifier = tmp_mod))
+			{
+				numOfDuplicates++;
+				if (numOfDuplicates > 1)
+					debug_fatal ( "Duplicate key+modifier combination (%i) (%i)",tmp_key,tmp_mod );
+			}
+		}
+		numOfDuplicates = 0;
+	}
+
 	return 0;
 }
 
@@ -55,273 +238,25 @@ int check_for_validity ( void )
 // Author:		Retro
 //
 // Description:		returns direct-input-keypress (DIK) on input-string.
-//					there are no else-ifs here, as only 127 else ifs can
-//					follow one if.. have the find some other method of
-//					checking for wrong input..
+//					uses the all_keys array defined above
 //
 //*********************************************************************/
 int get_DIK_from_string ( char* keystring )
 {
+	int i;
 	int retval = 0;
 
-	if ( strcmp ( keystring, "ESCAPE" ) == 0 )
-		retval = 0x01;
-	if ( strcmp ( keystring, "1" ) == 0 )
-		retval = 0x02;
-	if ( strcmp ( keystring, "2" ) == 0 )
-		retval = 0x03;
-	if ( strcmp ( keystring, "3" ) == 0 )
-		retval = 0x04;
-	if ( strcmp ( keystring, "4" ) == 0 )
-		retval = 0x05;
-	if ( strcmp ( keystring, "5" ) == 0 )
-		retval = 0x06;
-	if ( strcmp ( keystring, "6" ) == 0 )
-		retval = 0x07;
-	if ( strcmp ( keystring, "7" ) == 0 )
-		retval = 0x08;
-	if ( strcmp ( keystring, "8" ) == 0 )
-		retval = 0x09;
-	if ( strcmp ( keystring, "9" ) == 0 )
-		retval = 0x0A;
-	if ( strcmp ( keystring, "0" ) == 0 )
-		retval = 0x0B;
-	if ( strcmp ( keystring, "MINUS" ) == 0 )
-		retval =  0x0C;  /* - on main keyboard */
-	if ( strcmp ( keystring, "EQUALS" ) == 0 )
-		retval = 0x0D;
-	if (( strcmp ( keystring, "BACK" ) == 0 ) || ( strcmp ( keystring, "BACKSPACE" ) == 0 ))
-		retval = 0x0E;    /* backspace */
-	if ( strcmp ( keystring, "TAB" ) == 0 )
-		retval = 0x0F;
-	if ( strcmp ( keystring, "Q" ) == 0 )
-		retval = 0x10;
-	if ( strcmp ( keystring, "W" ) == 0 )
-		retval = 0x11;
-	if ( strcmp ( keystring, "E" ) == 0 )
-		retval = 0x12;
-	if ( strcmp ( keystring, "R" ) == 0 )
-		retval = 0x13;
-	if ( strcmp ( keystring, "T" ) == 0 )
-		retval = 0x14;
-	if ( strcmp ( keystring, "Y" ) == 0 )
-		retval = 0x15;
-	if ( strcmp ( keystring, "U" ) == 0 )
-		retval = 0x16;
-	if ( strcmp ( keystring, "I" ) == 0 )
-		retval = 0x17;
-	if ( strcmp ( keystring, "O" ) == 0 )
-		retval = 0x18;
-	if ( strcmp ( keystring, "P" ) == 0 )
-		retval = 0x19;
-	if ( strcmp ( keystring, "LBRACKET" ) == 0 )
-		retval = 0x1A;
-	if ( strcmp ( keystring, "RBRACKET" ) == 0 )
-		retval = 0x1B;
-	if ( strcmp ( keystring, "RETURN" ) == 0 )
-		retval = 0x1C;    /* Enter on main keyboard */
-	if ( strcmp ( keystring, "LCONTROL" ) == 0 )
-		retval = 0x1D;
-	if ( strcmp ( keystring, "A" ) == 0 )
-		retval = 0x1E;
-	if ( strcmp ( keystring, "S" ) == 0 )
-		retval = 0x1F;
-	if ( strcmp ( keystring, "D" ) == 0 )
-		retval = 0x20;
-	if ( strcmp ( keystring, "F" ) == 0 )
-		retval = 0x21;
-	if ( strcmp ( keystring, "G" ) == 0 )
-		retval = 0x22;
-	if ( strcmp ( keystring, "H" ) == 0 )
-		retval = 0x23;
-	if ( strcmp ( keystring, "J" ) == 0 )
-		retval = 0x24;
-	if ( strcmp ( keystring, "K" ) == 0 )
-		retval = 0x25;
-	if ( strcmp ( keystring, "L" ) == 0 )
-		retval = 0x26;
-	if ( strcmp ( keystring, "SEMICOLON" ) == 0 )
-		retval = 0x27;
-	if ( strcmp ( keystring, "APOSTROPHE" ) == 0 )
-		retval = 0x28;
-	if ( strcmp ( keystring, "GRAVE" ) == 0 )
-		retval = 0x29;    /* accent grave */
-	if ( strcmp ( keystring, "LSHIFT" ) == 0 )
-		retval = 0x2A;
-	if ( strcmp ( keystring, "BACKSLASH" ) == 0 )
-		retval = 0x2B;
-	if ( strcmp ( keystring, "Z" ) == 0 )
-		retval = 0x2C;
-	if ( strcmp ( keystring, "X" ) == 0 )
-		retval = 0x2D;
-	if ( strcmp ( keystring, "C" ) == 0 )
-		retval = 0x2E;
-	if ( strcmp ( keystring, "V" ) == 0 )
-		retval = 0x2F;
-	if ( strcmp ( keystring, "B" ) == 0 )
-		retval = 0x30;
-	if ( strcmp ( keystring, "N" ) == 0 )
-		retval = 0x31;
-	if ( strcmp ( keystring, "M" ) == 0 )
-		retval = 0x32;
-	if ( strcmp ( keystring, "COMMA" ) == 0 )
-		retval = 0x33;
-	if ( strcmp ( keystring, "PERIOD" ) == 0 )
-		retval = 0x34;    /* . on main keyboard */
-	if ( strcmp ( keystring, "SLASH" ) == 0 )
-		retval = 0x35;    /* / on main keyboard */
-	if ( strcmp ( keystring, "RSHIFT" ) == 0 )
-		retval = 0x36;
-	if (( strcmp ( keystring, "MULTIPLY" ) == 0 ) || ( strcmp ( keystring, "NUMPADSTAR" ) == 0 ))
-		retval = 0x37;    /* * on numeric keypad */
-	if (( strcmp ( keystring, "LMENU" ) == 0 ) || ( strcmp ( keystring, "LALT" ) == 0 ))
-		retval = 0x38;    /* left Alt */
-	if ( strcmp ( keystring, "SPACE" ) == 0 )
-		retval = 0x39;
-	if (( strcmp ( keystring, "CAPITAL" ) == 0 ) || ( strcmp ( keystring, "CAPSLOCK" ) == 0 ))
-		retval = 0x3A;
-	if ( strcmp ( keystring, "F1" ) == 0 )
-		retval = 0x3B;
-	if ( strcmp ( keystring, "F2" ) == 0 )
-		retval = 0x3C;
-	if ( strcmp ( keystring, "F3" ) == 0 )
-		retval = 0x3D;
-	if ( strcmp ( keystring, "F4" ) == 0 )
-		retval = 0x3E;
-	if ( strcmp ( keystring, "F5" ) == 0 )
-		retval = 0x3F;
-	if ( strcmp ( keystring, "F6" ) == 0 )
-		retval = 0x40;
-	if ( strcmp ( keystring, "F7" ) == 0 )
-		retval = 0x41;
-	if ( strcmp ( keystring, "F8" ) == 0 )
-		retval = 0x42;
-	if ( strcmp ( keystring, "F9" ) == 0 )
-		retval = 0x43;
-	if ( strcmp ( keystring, "F10" ) == 0 )
-		retval = 0x44;
-	if ( strcmp ( keystring, "NUMLOCK" ) == 0 )
-		retval = 0x45;
-	if ( strcmp ( keystring, "SCROLL" ) == 0 )
-		retval = 0x46;    /* Scroll Lock */
-	if ( strcmp ( keystring, "NUMPAD7" ) == 0 )
-		retval = 0x47;
-	if ( strcmp ( keystring, "NUMPAD8" ) == 0 )
-		retval = 0x48;
-	if ( strcmp ( keystring, "NUMPAD9" ) == 0 )
-		retval = 0x49;
-	if (( strcmp ( keystring, "SUBTRACT" ) == 0 ) || ( strcmp ( keystring, "NUMPADMINUS" ) == 0 ))
-		retval = 0x4A;    /* - on numeric keypad */
-	if ( strcmp ( keystring, "NUMPAD4" ) == 0 )
-		retval = 0x4B;
-	if ( strcmp ( keystring, "NUMPAD5" ) == 0 )
-		retval = 0x4C;
-	if ( strcmp ( keystring, "NUMPAD6" ) == 0 )
-		retval = 0x4D;
-	if (( strcmp ( keystring, "ADD" ) == 0 ) || ( strcmp ( keystring, "NUMPADPLUS" ) == 0 ))
-		retval = 0x4E;    /* + on numeric keypad */
-	if ( strcmp ( keystring, "NUMPAD1" ) == 0 )
-		retval = 0x4F;
-	if ( strcmp ( keystring, "NUMPAD2" ) == 0 )
-		retval = 0x50;
-	if ( strcmp ( keystring, "NUMPAD3" ) == 0 )
-		retval = 0x51;
-	if ( strcmp ( keystring, "NUMPAD0" ) == 0 )
-		retval = 0x52;
-	if (( strcmp ( keystring, "DECIMAL" ) == 0 ) || ( strcmp ( keystring, "NUMPADPERIOD" ) == 0 ))
-		retval = 0x53;    /* . on numeric keypad */
-	if ( strcmp ( keystring, "OEM_102" ) == 0 )
-		retval = 0x56;    /* < > | on UK/Germany keyboards */
-	if ( strcmp ( keystring, "F11" ) == 0 )
-		retval = 0x57;
-	if ( strcmp ( keystring, "F12" ) == 0 )
-		retval = 0x58;
-	if ( strcmp ( keystring, "F13" ) == 0 )
-			retval = 0x64;    /*                     (NEC PC98) */
-	if ( strcmp ( keystring, "F14" ) == 0 )
-			retval = 0x65;    /*                     (NEC PC98) */
-	if ( strcmp ( keystring, "F15" ) == 0 )
-			retval = 0x66;    /*                     (NEC PC98) */
-	if ( strcmp ( keystring, "KANA" ) == 0 )
-			retval = 0x70;    /* (Japanese keyboard)            */
-	if ( strcmp ( keystring, "ABNT_C1" ) == 0 )
-			retval = 0x73;    /* / ? on Portugese (Brazilian) keyboards */
-	if ( strcmp ( keystring, "CONVERT" ) == 0 )
-			retval = 0x79;    /* (Japanese keyboard)            */
-	if ( strcmp ( keystring, "NOCONVERT" ) == 0 )
-			retval = 0x7B;    /* (Japanese keyboard)            */
-	if ( strcmp ( keystring, "YEN" ) == 0 )
-			retval = 0x7D;    /* (Japanese keyboard)            */
-	if ( strcmp ( keystring, "ABNT_C2" ) == 0 )
-			retval = 0x7E;    /* Numpad . on Portugese (Brazilian) keyboards */
-	if ( strcmp ( keystring, "NUMPADEQUALS" ) == 0 )
-			retval = 0x8D;    /* = on numeric keypad (NEC PC98) */
-	if ( strcmp ( keystring, "CIRCUMFLEX" ) == 0 )
-			retval = 0x90;    /* (Japanese keyboard)            */
-	if ( strcmp ( keystring, "AT" ) == 0 )
-			retval = 0x91;    /*                     (NEC PC98) */
-	if ( strcmp ( keystring, "COLON" ) == 0 )
-			retval = 0x92;    /*                     (NEC PC98) */
-	if ( strcmp ( keystring, "UNDERLINE" ) == 0 )
-			retval = 0x93;    /*                     (NEC PC98) */
-	if ( strcmp ( keystring, "KANJI" ) == 0 )
-			retval = 0x94;    /* (Japanese keyboard)            */
-	if ( strcmp ( keystring, "STOP" ) == 0 )
-			retval = 0x95;    /*                     (NEC PC98) */
-	if ( strcmp ( keystring, "AX" ) == 0 )
-			retval = 0x96;    /*                     (Japan AX) */
-	if ( strcmp ( keystring, "UNLABELED" ) == 0 )
-			retval = 0x97;    /*                        (J3100) */
-	if ( strcmp ( keystring, "NUMPADENTER" ) == 0 )
-			retval = 0x9C;    /* Enter on numeric keypad */
-	if ( strcmp ( keystring, "RCONTROL" ) == 0 )
-			retval = 0x9D;
-	if ( strcmp ( keystring, "NUMPADCOMMA" ) == 0 )
-			retval = 0xB3;    /* , on numeric keypad (NEC PC98) */
-	if (( strcmp ( keystring, "DIVIDE" ) == 0 ) || ( strcmp ( keystring, "NUMPADSLASH" ) == 0 ))
-			retval = 0xB5;    /* / on numeric keypad */
-	if ( strcmp ( keystring, "SYSRQ" ) == 0 )
-			retval = 0xB7;
-	if (( strcmp ( keystring, "RMENU" ) == 0 ) || ( strcmp ( keystring, "RALT" ) == 0 ))
-			retval = 0xB8;    /* right Alt */
-	if ( strcmp ( keystring, "PAUSE" ) == 0 )
-			retval = 0xC5;    /* Pause */
-	if ( strcmp ( keystring, "HOME" ) == 0 )
-			retval = 0xC7;    /* Home on arrow keypad */
-	if (( strcmp ( keystring, "UP" ) == 0 ) || ( strcmp ( keystring, "UPARROW" ) == 0 ))
-			retval = 0xC8;    /* UpArrow on arrow keypad */
-	if (( strcmp ( keystring, "PRIOR" ) == 0 ) || ( strcmp ( keystring, "PGUP" ) == 0 ))
-			retval = 0xC9;    /* PgUp on arrow keypad */
-	if (( strcmp ( keystring, "LEFT" ) == 0 ) || ( strcmp ( keystring, "LEFTARROW" ) == 0 ))
-			retval = 0xCB;    /* LeftArrow on arrow keypad */
-	if (( strcmp ( keystring, "RIGHT" ) == 0 ) || ( strcmp ( keystring, "RIGHTARROW" ) == 0 ))
-			retval = 0xCD;    /* RightArrow on arrow keypad */
-	if ( strcmp ( keystring, "END" ) == 0 )
-			retval = 0xCF;    /* End on arrow keypad */
-	if (( strcmp ( keystring, "DOWN" ) == 0 ) || ( strcmp ( keystring, "DOWNARROW" ) == 0 ))
-			retval = 0xD0;    /* DownArrow on arrow keypad */
-	if (( strcmp ( keystring, "NEXT" ) == 0 ) || ( strcmp ( keystring, "PGDN" ) == 0 ))
-			retval = 0xD1;    /* PgDn on arrow keypad */
-	if ( strcmp ( keystring, "INSERT" ) == 0 )
-			retval = 0xD2;    /* Insert on arrow keypad */
-	if ( strcmp ( keystring, "DELETE" ) == 0 )
-			retval = 0xD3;    /* Delete on arrow keypad */
-	if ( strcmp ( keystring, "LWIN" ) == 0 )
-			retval = 0xDB;    /* Left Windows key */
-	if ( strcmp ( keystring, "RWIN" ) == 0 )
-			retval = 0xDC;    /* Right Windows key */
-	if ( strcmp ( keystring, "APPS" ) == 0 )
-			retval = 0xDD;    /* AppMenu key */
-	if ( strcmp ( keystring, "POWER" ) == 0 )
-			retval = 0xDE;
-	if ( strcmp ( keystring, "SLEEP" ) == 0 )
-			retval = 0xDF;
-	if ( strcmp ( keystring, "WAKE" ) == 0 )
-			retval = 0xE3;
-
+	for ( i = 0; i < NUM_OF_KEYS; i++ )
+	{
+		if ( strcmp(all_keys[i].key_name,keystring) == 0 )
+		{
+			retval = all_keys[i].dik_val;
+			break;
+		}
+	}
 	if ( retval == 0 )	// kinda crappy way of error checking.. if reval is still 0, obviously no string matched.
 	{
-		debug_fatal("Don´t know key %s",keystring);
+		debug_fatal ( "Don´t know key %s",keystring );
 	}
 
 	return retval;
@@ -454,7 +389,12 @@ void read_mapping_from_file ( char* filename )
 						case 1: sprintf(key,"%s",buffer);break;
 						case 2: sprintf(modifier,"%s",buffer);break;
 						case 3: sprintf(state,"%s",buffer);break;
-						default: assert ( 0 );break;
+						default: 
+#ifdef TIGHT_ARSE_DEBUG
+							debug_fatal ( "switch defaulted" );break;
+#else
+							assert ( 0 );break;
+#endif
 					}
 					//**********************************************************************
 					//	i%4 == 3 means 'end of line'; copying the data into the struct
@@ -470,18 +410,30 @@ void read_mapping_from_file ( char* filename )
 				}
 				else	// a read string is bigger than SLIZE
 				{
+#ifdef TIGHT_ARSE_DEBUG
+					debug_fatal ( "a read string is bigger than SLIZE" );
+#else
 					assert ( 0 );
+#endif
 					break;
 				}
 			}
 			else	// EOF or fscanf error.. (nothing read)
+			{
+#ifdef TIGHT_ARSE_DEBUG
+				if (strlen(buffer) == 0)
+					debug_fatal ( "a read string is of zero length" );
+#endif
 				break;
+			}
 			i++;
 		}
 		fclose(fp);
 
 		number_of_commands = line;
 	}
+
+//	check_for_validity ();
 }
 
 //**********************************************************************
