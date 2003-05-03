3956846b3a2ada1146abf5e31fc8e750775c81d7
yem <yem>
2003-05-03 13:06:12 +0000

    Added a hack to remove '-I.' from the CFLAGS when building modules/system and modules/system/external. This is to work around time.h in those locations that conflicts with the system time.h.

--- autogen.sh
+++ autogen.sh
@@ -6,3 +6,10 @@ aclocal \
 && autoconf \
 && automake --add-missing --copy --foreign
 
+# Hack to ensure "-I." doesn't appear in the CFLAGS in these two directories.
+# Otherwise the time.h present there conflicts with the system <time.h>.
+for i in modules/system/Makefile.in modules/system/external/Makefile.in; do
+    if [ -f "$i" ]; then
+        perl -p -i -e 's/^(DEFS =.* )-I\. -I\$\(srcdir\)/$1/' "$i"
+    fi
+done
