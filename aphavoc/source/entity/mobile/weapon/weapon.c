00eb880cccfec0d886018336f23aaeb6ade21296
Jabberwock <Jabberwock>
2005-01-31 23:40:37 +0000

    Jabberwock - corrected bugs in cannon tracking

--- aphavoc/source/entity/mobile/weapon/weapon.c
+++ aphavoc/source/entity/mobile/weapon/weapon.c
@@ -1,62 +1,62 @@
-// 
+//
 // 	 Enemy Engaged RAH-66 Comanche Versus KA-52 Hokum
 // 	 Copyright (C) 2000 Empire Interactive (Europe) Ltd,
 // 	 677 High Road, North Finchley, London N12 0DA
-// 
+//
 // 	 Please see the document LICENSE.TXT for the full licence agreement
-// 
+//
 // 2. LICENCE
-//  2.1 	
-//  	Subject to the provisions of this Agreement we now grant to you the 
+//  2.1
+//  	Subject to the provisions of this Agreement we now grant to you the
 //  	following rights in respect of the Source Code:
-//   2.1.1 
-//   	the non-exclusive right to Exploit  the Source Code and Executable 
-//   	Code on any medium; and 
-//   2.1.2 
+//   2.1.1
+//   	the non-exclusive right to Exploit  the Source Code and Executable
+//   	Code on any medium; and
+//   2.1.2
 //   	the non-exclusive right to create and distribute Derivative Works.
-//  2.2 	
+//  2.2
 //  	Subject to the provisions of this Agreement we now grant you the
 // 	following rights in respect of the Object Code:
-//   2.2.1 
+//   2.2.1
 // 	the non-exclusive right to Exploit the Object Code on the same
 // 	terms and conditions set out in clause 3, provided that any
 // 	distribution is done so on the terms of this Agreement and is
 // 	accompanied by the Source Code and Executable Code (as
 // 	applicable).
-// 
+//
 // 3. GENERAL OBLIGATIONS
-//  3.1 
+//  3.1
 //  	In consideration of the licence granted in clause 2.1 you now agree:
-//   3.1.1 
+//   3.1.1
 // 	that when you distribute the Source Code or Executable Code or
 // 	any Derivative Works to Recipients you will also include the
 // 	terms of this Agreement;
-//   3.1.2 
+//   3.1.2
 // 	that when you make the Source Code, Executable Code or any
 // 	Derivative Works ("Materials") available to download, you will
 // 	ensure that Recipients must accept the terms of this Agreement
 // 	before being allowed to download such Materials;
-//   3.1.3 
+//   3.1.3
 // 	that by Exploiting the Source Code or Executable Code you may
 // 	not impose any further restrictions on a Recipient's subsequent
 // 	Exploitation of the Source Code or Executable Code other than
 // 	those contained in the terms and conditions of this Agreement;
-//   3.1.4 
+//   3.1.4
 // 	not (and not to allow any third party) to profit or make any
 // 	charge for the Source Code, or Executable Code, any
 // 	Exploitation of the Source Code or Executable Code, or for any
 // 	Derivative Works;
-//   3.1.5 
-// 	not to place any restrictions on the operability of the Source 
+//   3.1.5
+// 	not to place any restrictions on the operability of the Source
 // 	Code;
-//   3.1.6 
+//   3.1.6
 // 	to attach prominent notices to any Derivative Works stating
 // 	that you have changed the Source Code or Executable Code and to
 // 	include the details anddate of such change; and
-//   3.1.7 
+//   3.1.7
 //   	not to Exploit the Source Code or Executable Code otherwise than
 // 	as expressly permitted by  this Agreement.
-// 
+//
 
 
 
@@ -1309,54 +1309,53 @@ void update_entity_weapon_systems (entity *source)
 
 							set_local_entity_int_value (source, INT_TYPE_SELECTED_WEAPON_SYSTEM_READY, FALSE);
 						}
-					}
 
+						// 050121 Jabberwock - Cannon tracking with limits - thanks, Loke!
 
-					// 050121 Jabberwock - Cannon tracking with limits - thanks, Loke!
-
-					if (command_line_cannontrack)
-					{
-						switch (command_line_cannontrack)
+						if ((command_line_cannontrack) && (source == get_gunship_entity())) // Jabberwock 050128 Bug removed!
 						{
-							case 1:
+							switch (command_line_cannontrack)
 							{
-								if (target_acquisition_system == TARGET_ACQUISITION_SYSTEM_OFF)
+								case 1:
 								{
-									required_heading_offset = -pilot_head_heading;
+									if (target_acquisition_system == TARGET_ACQUISITION_SYSTEM_OFF)
+									{
+										required_heading_offset = -pilot_head_heading;
 
-									required_pitch_offset = pilot_head_pitch;
+										required_pitch_offset = pilot_head_pitch;
+									}
+									break;
 								}
-								break;
-							}
 
-							case 2:
-							{
-								if ((target_acquisition_system == TARGET_ACQUISITION_SYSTEM_IHADSS) || (target_acquisition_system == TARGET_ACQUISITION_SYSTEM_HIDSS) || (target_acquisition_system == TARGET_ACQUISITION_SYSTEM_HMS))
+								case 2:
 								{
-									required_heading_offset = -pilot_head_heading;
+									if ((target_acquisition_system == TARGET_ACQUISITION_SYSTEM_IHADSS) || (target_acquisition_system == TARGET_ACQUISITION_SYSTEM_HIDSS) || (target_acquisition_system == TARGET_ACQUISITION_SYSTEM_HMS))
+									{
+										required_heading_offset = -pilot_head_heading;
 
-									required_pitch_offset = pilot_head_pitch;
+										required_pitch_offset = pilot_head_pitch;
+									}
+									break;
 								}
-								break;
+
 							}
+							required_heading_offset = bound
+							(
+								required_heading_offset,
+								weapon_config_database[config_type][package].min_heading_limit,
+								weapon_config_database[config_type][package].max_heading_limit
+							);
+
+							required_pitch_offset = bound
+							(
+								required_pitch_offset,
+								weapon_config_database[config_type][package].min_pitch_limit,
+								weapon_config_database[config_type][package].max_pitch_limit
+							);
 
 						}
-						required_heading_offset = bound
-						(
-							required_heading_offset,
-							weapon_config_database[config_type][package].min_heading_limit,
-							weapon_config_database[config_type][package].max_heading_limit
-						);
-
-						required_pitch_offset = bound
-						(
-							required_pitch_offset,
-							weapon_config_database[config_type][package].min_pitch_limit,
-							weapon_config_database[config_type][package].max_pitch_limit
-						);
-
-					}
 					// 050121 Jabberwock ends
+					}
 
 						//set_local_entity_int_value (source, INT_TYPE_SELECTED_WEAPON_SYSTEM_READY, FALSE);
 
