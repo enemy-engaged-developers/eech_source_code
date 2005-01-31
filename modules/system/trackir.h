4a436403560640492c755ca3cad546454a03e8f3
Retro <Retro>
2005-01-31 22:55:22 +0000

    Code to hookup my TrackIR.dll (which can be found in the setup/cohokum directory)
    This is no stub file anymore, everybody can now build a TIR-enhanced release build.

--- modules/system/trackir.h
+++ modules/system/trackir.h
@@ -1,13 +1,25 @@
 /* CRH 030320 */
 /* quick hack to get compile working with OpenWatcom */
 
-extern int query_TIR_active ( void );
+extern void __cdecl InitTrackIR ( void );
+extern void __cdecl ExitTrackIR ( void );
 
-extern int GetYaw ( void );
+extern int __cdecl query_TIR_active ( void );
+extern void TIR_SetTimeout(unsigned int frames);
 
-extern int GetPitch ( void );
+extern void TIR_Poll();
+extern float TIR_GetYaw();
+extern float TIR_GetPitch();
+extern float TIR_GetRoll();
+extern float TIR_GetX();
+extern float TIR_GetY();
+extern float TIR_GetZ();
 
-extern void InitTrackIR ( void );
+extern void TIR_SetMaxXY(const unsigned int value);
+extern float TIR_GetMaxXY();
 
-extern void ExitTrackIR ( void );
+extern int __cdecl GetYaw ( void );
+extern int __cdecl GetPitch ( void );
+
+
 
