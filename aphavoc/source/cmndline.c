27963d6abe6a3f685aeebfd885ca917b771aeab3
gotcha <gotcha>
2005-10-28 15:26:54 +0000

    //VJ 051028 made z-buffer calculation for landing strips optional, winter season global var

--- aphavoc/source/cmndline.c
+++ aphavoc/source/cmndline.c
@@ -209,6 +209,8 @@ int
 	global_anisotropic								= 0,	//VJ 050530 AF on/off
 	global_PNVS_level									= 0,  // VJ 050704 light setting for PNVS
 	global_dynamic_water								= 0,  // VJ 050817 dynamic water textures	
+	global_zbuffer										= 0,	//VJ 051011 zbuffer correction
+	global_season										= 1,	//VJ 051011 add winter textures
 	command_line_texture_colour					= 0,  //VJ 050303 texture mod using the colours of the terrain texture bitmaps directly
 	command_line_autosave					= 0,  //Casm 17JUN05 Autosave option
 	command_line_cannontrack						= 1, // Jabberwock 050120 Cannon tracking
