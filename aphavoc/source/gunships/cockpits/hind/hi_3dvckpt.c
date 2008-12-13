22cb19dce24029dcc37572992e7a7ed64792436f
arneh <arneh>
2008-12-13 18:31:57 +0000

    Initialize canopy_door_state.

--- aphavoc/source/gunships/cockpits/hind/hi_3dvckpt.c
+++ aphavoc/source/gunships/cockpits/hind/hi_3dvckpt.c
@@ -558,6 +558,7 @@ void initialise_hind_3d_cockpit (void)
 
 	open_door = !get_local_entity_int_value(get_gunship_entity(), INT_TYPE_AIRBORNE_AIRCRAFT) && command_line_dynamics_engine_startup;
 	door_state = get_local_entity_int_value(get_gunship_entity(), INT_TYPE_AIRBORNE_AIRCRAFT) ? 1.0 : 0.0;
+	canopy_door_state = 1.0 - door_state;
 	animate_keyframed_sub_object_type(virtual_cockpit_pilot_door_inst3d, OBJECT_3D_SUB_OBJECT_CANOPY_DOORS, door_state);
 	animate_keyframed_sub_object_type(virtual_cockpit_external_inst3d, OBJECT_3D_SUB_OBJECT_CANOPY_DOORS, door_state);
 	animate_keyframed_sub_object_type(virtual_cockpit_canopy_inst3d, OBJECT_3D_SUB_OBJECT_CANOPY_DOORS, door_state);
