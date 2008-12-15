a0c12f9e6f922eb10070a67811511ce2cd6bd74a
arneh <arneh>
2008-12-15 21:40:43 +0000

    Re-instate code accidentally removed. Fixes bug with next/prev target not working in hind and havoc.

--- aphavoc/source/gunships/avionics/havoc/ha_eo.c
+++ aphavoc/source/gunships/avionics/havoc/ha_eo.c
@@ -1,62 +1,62 @@
-// 
+//
 // 	 Enemy Engaged RAH-66 Comanche Versus KA-52 Hokum
 // 	 Copyright (C) 2000 Empire Interactive (Europe) Ltd,
 // 	 677 High Road, North Finchley, London N12 0DA
-// 
+//
 // 	 Please see the document LICENSE.TXT for the full licence agreement
-// 
+//
 // 2. LICENCE
-//  2.1 	
-//  	Subject to the provisions of this Agreement we now grant to you the 
+//  2.1
+//  	Subject to the provisions of this Agreement we now grant to you the
 //  	following rights in respect of the Source Code:
-//   2.1.1 
-//   	the non-exclusive right to Exploit  the Source Code and Executable 
-//   	Code on any medium; and 
-//   2.1.2 
+//   2.1.1
+//   	the non-exclusive right to Exploit  the Source Code and Executable
+//   	Code on any medium; and
+//   2.1.2
 //   	the non-exclusive right to create and distribute Derivative Works.
-//  2.2 	
+//  2.2
 //  	Subject to the provisions of this Agreement we now grant you the
 // 	following rights in respect of the Object Code:
-//   2.2.1 
+//   2.2.1
 // 	the non-exclusive right to Exploit the Object Code on the same
 // 	terms and conditions set out in clause 3, provided that any
 // 	distribution is done so on the terms of this Agreement and is
 // 	accompanied by the Source Code and Executable Code (as
 // 	applicable).
-// 
+//
 // 3. GENERAL OBLIGATIONS
-//  3.1 
+//  3.1
 //  	In consideration of the licence granted in clause 2.1 you now agree:
-//   3.1.1 
+//   3.1.1
 // 	that when you distribute the Source Code or Executable Code or
 // 	any Derivative Works to Recipients you will also include the
 // 	terms of this Agreement;
-//   3.1.2 
+//   3.1.2
 // 	that when you make the Source Code, Executable Code or any
 // 	Derivative Works ("Materials") available to download, you will
 // 	ensure that Recipients must accept the terms of this Agreement
 // 	before being allowed to download such Materials;
-//   3.1.3 
+//   3.1.3
 // 	that by Exploiting the Source Code or Executable Code you may
 // 	not impose any further restrictions on a Recipient's subsequent
 // 	Exploitation of the Source Code or Executable Code other than
 // 	those contained in the terms and conditions of this Agreement;
-//   3.1.4 
+//   3.1.4
 // 	not (and not to allow any third party) to profit or make any
 // 	charge for the Source Code, or Executable Code, any
 // 	Exploitation of the Source Code or Executable Code, or for any
 // 	Derivative Works;
-//   3.1.5 
-// 	not to place any restrictions on the operability of the Source 
+//   3.1.5
+// 	not to place any restrictions on the operability of the Source
 // 	Code;
-//   3.1.6 
+//   3.1.6
 // 	to attach prominent notices to any Derivative Works stating
 // 	that you have changed the Source Code or Executable Code and to
 // 	include the details anddate of such change; and
-//   3.1.7 
+//   3.1.7
 //   	not to Exploit the Source Code or Executable Code otherwise than
 // 	as expressly permitted by  this Agreement.
-// 
+//
 
 
 
@@ -285,7 +285,7 @@ void update_havoc_eo (eo_params *eo)
 			medium_slew_rate = rad (0.25) * get_delta_time ();
 
 			mouse_slew_rate = rad (0.6) * get_delta_time ();	// Jabberwock 030930
-			
+
 			coarse_slew_rate = rad (1.0) * get_delta_time ();
 
 			break;
@@ -299,7 +299,7 @@ void update_havoc_eo (eo_params *eo)
 			medium_slew_rate = rad (2.5) * get_delta_time ();
 
 			mouse_slew_rate = rad (6) * get_delta_time ();	// Jabberwock 030930
-			
+
 			coarse_slew_rate = rad (10.0) * get_delta_time ();
 
 			break;
@@ -313,7 +313,7 @@ void update_havoc_eo (eo_params *eo)
 			medium_slew_rate = rad (20.0) * get_delta_time ();
 
 			mouse_slew_rate = rad (48) * get_delta_time ();	// Jabberwock 030930
-			
+
 			coarse_slew_rate = rad (80.0) * get_delta_time ();
 
 			break;
@@ -330,9 +330,42 @@ void update_havoc_eo (eo_params *eo)
 
 	keyboard_slew_eo_system(fine_slew_rate, medium_slew_rate, coarse_slew_rate);
 
+	while (single_target_acquisition_system_select_next_target_key)
+	{
+		select_next_eo_target ();
+
+		single_target_acquisition_system_select_next_target_key--;
+	}
+
+	////////////////////////////////////////
+
+	while (single_target_acquisition_system_select_previous_target_key)
+	{
+		select_previous_eo_target ();
+
+		single_target_acquisition_system_select_previous_target_key--;
+	}
+
+// Jabberwock 031107 Designated targets
+
+	while (single_target_acquisition_system_select_next_designated_key)
+	{
+		select_next_designated_eo_target ();
+
+		single_target_acquisition_system_select_next_designated_key--;
+	}
+
+	////////////////////////////////////////
+
+	while (single_target_acquisition_system_select_previous_designated_key)
+	{
+		select_previous_designated_eo_target ();
+
+		single_target_acquisition_system_select_previous_designated_key--;
+	}
 	// Jabberwock 030930 - Mouse FLIR control functions
 
-	if (mouse_move_left) 
+	if (mouse_move_left)
 	{
 		eo_azimuth -= mouse_slew_rate;
 
@@ -341,7 +374,7 @@ void update_havoc_eo (eo_params *eo)
 		mouse_move_left--;
 	}
 
-	if (mouse_move_right) 
+	if (mouse_move_right)
 	{
 		eo_azimuth += mouse_slew_rate;
 
@@ -350,7 +383,7 @@ void update_havoc_eo (eo_params *eo)
 		mouse_move_right--;
 	}
 
-	if (mouse_move_up) 
+	if (mouse_move_up)
 	{
 		eo_elevation -= medium_slew_rate;
 
@@ -359,7 +392,7 @@ void update_havoc_eo (eo_params *eo)
 		mouse_move_up--;
 	}
 
-	if (mouse_move_down) 
+	if (mouse_move_down)
 	{
 		eo_elevation += medium_slew_rate;
 
