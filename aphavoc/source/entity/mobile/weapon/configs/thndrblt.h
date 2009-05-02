a137aca55b27898d47d633ad79cf7167b9e5da5d
arneh <arneh>
2009-05-02 12:33:49 +0000

    Replaced most of the rocket pods with guided missiles.

--- aphavoc/source/entity/mobile/weapon/configs/thndrblt.h
+++ aphavoc/source/entity/mobile/weapon/configs/thndrblt.h
@@ -1,62 +1,62 @@
-// 
+//
 // 	 Enemy Engaged RAH-66 Comanche Versus KA-52 Hokum
 // 	 Copyright (C) 2000 Empire Interactive (Europe) Ltd,
 // 	 677 High Road, North Finchley, London N12 0DA
-// 
+//
 // 	 Please see the document LICENSE.TXT for the full licence agreement
-// 
+//
 // 2. LICENCE
-//  2.1 	
-//  	Subject to the provisions of this Agreement we now grant to you the 
+//  2.1
+//  	Subject to the provisions of this Agreement we now grant to you the
 //  	following rights in respect of the Source Code:
-//   2.1.1 
-//   	the non-exclusive right to Exploit  the Source Code and Executable 
-//   	Code on any medium; and 
-//   2.1.2 
+//   2.1.1
+//   	the non-exclusive right to Exploit  the Source Code and Executable
+//   	Code on any medium; and
+//   2.1.2
 //   	the non-exclusive right to create and distribute Derivative Works.
-//  2.2 	
+//  2.2
 //  	Subject to the provisions of this Agreement we now grant you the
 // 	following rights in respect of the Object Code:
-//   2.2.1 
+//   2.2.1
 // 	the non-exclusive right to Exploit the Object Code on the same
 // 	terms and conditions set out in clause 3, provided that any
 // 	distribution is done so on the terms of this Agreement and is
 // 	accompanied by the Source Code and Executable Code (as
 // 	applicable).
-// 
+//
 // 3. GENERAL OBLIGATIONS
-//  3.1 
+//  3.1
 //  	In consideration of the licence granted in clause 2.1 you now agree:
-//   3.1.1 
+//   3.1.1
 // 	that when you distribute the Source Code or Executable Code or
 // 	any Derivative Works to Recipients you will also include the
 // 	terms of this Agreement;
-//   3.1.2 
+//   3.1.2
 // 	that when you make the Source Code, Executable Code or any
 // 	Derivative Works ("Materials") available to download, you will
 // 	ensure that Recipients must accept the terms of this Agreement
 // 	before being allowed to download such Materials;
-//   3.1.3 
+//   3.1.3
 // 	that by Exploiting the Source Code or Executable Code you may
 // 	not impose any further restrictions on a Recipient's subsequent
 // 	Exploitation of the Source Code or Executable Code other than
 // 	those contained in the terms and conditions of this Agreement;
-//   3.1.4 
+//   3.1.4
 // 	not (and not to allow any third party) to profit or make any
 // 	charge for the Source Code, or Executable Code, any
 // 	Exploitation of the Source Code or Executable Code, or for any
 // 	Derivative Works;
-//   3.1.5 
-// 	not to place any restrictions on the operability of the Source 
+//   3.1.5
+// 	not to place any restrictions on the operability of the Source
 // 	Code;
-//   3.1.6 
+//   3.1.6
 // 	to attach prominent notices to any Derivative Works stating
 // 	that you have changed the Source Code or Executable Code and to
 // 	include the details anddate of such change; and
-//   3.1.7 
+//   3.1.7
 //   	not to Exploit the Source Code or Executable Code otherwise than
 // 	as expressly permitted by  this Agreement.
-// 
+//
 
 
 
@@ -91,6 +91,7 @@
 		#define NUM_CANNON_ROUNDS			(1174)
 		#define NUM_LAU69A_ROCKETS 		(19)
 		#define NUM_SIDEWINDER_MISSILES	(2)
+		#define NUM_MAVERICK_MISSILES   (1)
 		#define NUM_CHAFFS					(30)
 		#define NUM_FLARES	  	  			(30)
 
@@ -168,14 +169,14 @@
 			},
 			// WEAPON PACKAGE 4
 			{
-				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
-				NUM_LAU69A_ROCKETS,	 										// number
+				ENTITY_SUB_TYPE_WEAPON_AGM65D_MAVERICK,				// sub_type
+				NUM_MAVERICK_MISSILES,	 									// number
 				LHS_HARDPOINT1,												// heading_depth
 				LAU69A_ROCKET_POD,		  	 								// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -190,14 +191,14 @@
 			},
 			// WEAPON PACKAGE 5
 			{
-				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
-				NUM_LAU69A_ROCKETS,	 										// number
+				ENTITY_SUB_TYPE_WEAPON_AGM65D_MAVERICK,				// sub_type
+				NUM_MAVERICK_MISSILES,	 									// number
 				RHS_HARDPOINT1,												// heading_depth
 				LAU69A_ROCKET_POD,		  	 								// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -212,14 +213,14 @@
 			},
 			// WEAPON PACKAGE 6
 			{
-				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
-				NUM_LAU69A_ROCKETS,	 										// number
+				ENTITY_SUB_TYPE_WEAPON_AGM65D_MAVERICK,				// sub_type
+				NUM_MAVERICK_MISSILES,	 									// number
 				LHS_HARDPOINT2_1,												// heading_depth
 				LAU69A_ROCKET_POD,		  	 								// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -234,14 +235,14 @@
 			},
 			// WEAPON PACKAGE 7
 			{
-				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
-				NUM_LAU69A_ROCKETS,	 										// number
+				ENTITY_SUB_TYPE_WEAPON_AGM65D_MAVERICK,				// sub_type
+				NUM_MAVERICK_MISSILES,	 									// number
 				RHS_HARDPOINT2_1,												// heading_depth
 				LAU69A_ROCKET_POD,		  	 								// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -254,16 +255,17 @@
 				0,		 			 								  				// pitch_share_mask
 				MUZZLE_FLASH_INVALID,										// muzzle_flash_type
 			},
+#if 0
 			// WEAPON PACKAGE 8
 			{
-				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
-				NUM_LAU69A_ROCKETS,	 										// number
+				ENTITY_SUB_TYPE_WEAPON_AGM65D_MAVERICK,				// sub_type
+				NUM_MAVERICK_MISSILES,	 									// number
 				LHS_HARDPOINT2_2,												// heading_depth
 				LAU69A_ROCKET_POD,		  	 								// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -278,14 +280,14 @@
 			},
 			// WEAPON PACKAGE 9
 			{
-				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
-				NUM_LAU69A_ROCKETS,	 										// number
+				ENTITY_SUB_TYPE_WEAPON_AGM65D_MAVERICK,				// sub_type
+				NUM_MAVERICK_MISSILES,	 									// number
 				RHS_HARDPOINT2_2,												// heading_depth
 				LAU69A_ROCKET_POD,		  	 								// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -300,14 +302,14 @@
 			},
 			// WEAPON PACKAGE 10
 			{
-				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
-				NUM_LAU69A_ROCKETS,	 										// number
+				ENTITY_SUB_TYPE_WEAPON_AGM65D_MAVERICK,				// sub_type
+				NUM_MAVERICK_MISSILES,	 									// number
 				LHS_HARDPOINT2_3,												// heading_depth
 				LAU69A_ROCKET_POD,		  	 								// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -322,14 +324,14 @@
 			},
 			// WEAPON PACKAGE 11
 			{
-				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
-				NUM_LAU69A_ROCKETS,	 										// number
+				ENTITY_SUB_TYPE_WEAPON_AGM65D_MAVERICK,				// sub_type
+				NUM_MAVERICK_MISSILES,	 									// number
 				RHS_HARDPOINT2_3,												// heading_depth
 				LAU69A_ROCKET_POD,		  	 								// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -342,16 +344,25 @@
 				0,		 			 								  				// pitch_share_mask
 				MUZZLE_FLASH_INVALID,										// muzzle_flash_type
 			},
+#endif
+			// WEAPON PACKAGE 8
+			{ENTITY_SUB_TYPE_WEAPON_NO_WEAPON},
+			// WEAPON PACKAGE 9
+			{ENTITY_SUB_TYPE_WEAPON_NO_WEAPON},
+			// WEAPON PACKAGE 10
+			{ENTITY_SUB_TYPE_WEAPON_NO_WEAPON},
+			// WEAPON PACKAGE 11
+			{ENTITY_SUB_TYPE_WEAPON_NO_WEAPON},
 			// WEAPON PACKAGE 12
 			{
-				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
-				NUM_LAU69A_ROCKETS,	 										// number
+				ENTITY_SUB_TYPE_WEAPON_AGM65D_MAVERICK,				// sub_type
+				NUM_MAVERICK_MISSILES,	 									// number
 				LHS_HARDPOINT3,												// heading_depth
 				LAU69A_ROCKET_POD,		  	 								// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -366,14 +377,14 @@
 			},
 			// WEAPON PACKAGE 13
 			{
-				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
-				NUM_LAU69A_ROCKETS,	 										// number
+				ENTITY_SUB_TYPE_WEAPON_AGM65D_MAVERICK,				// sub_type
+				NUM_MAVERICK_MISSILES,	 									// number
 				RHS_HARDPOINT3,												// heading_depth
 				LAU69A_ROCKET_POD,		  	 								// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -388,14 +399,14 @@
 			},
 			// WEAPON PACKAGE 14
 			{
-				ENTITY_SUB_TYPE_WEAPON_AIM9M_SIDEWINDER,	  			// sub_type
-				NUM_SIDEWINDER_MISSILES,	 								// number
+				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
+				NUM_LAU69A_ROCKETS,	 										// number
 				LHS_HARDPOINT4,												// heading_depth
 				SIDEWINDER_PYLON,	  			  	 							// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				1,																	// salvo_size
+				4,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -410,14 +421,14 @@
 			},
 			// WEAPON PACKAGE 15
 			{
-				ENTITY_SUB_TYPE_WEAPON_AIM9M_SIDEWINDER,	  			// sub_type
-				NUM_SIDEWINDER_MISSILES,	 								// number
+				ENTITY_SUB_TYPE_WEAPON_LAU69A,	  						// sub_type
+				NUM_LAU69A_ROCKETS,	 										// number
 				RHS_HARDPOINT4,												// heading_depth
 				SIDEWINDER_PYLON,				  	 							// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
 				FALSE,															// rotate
-				1,																	// salvo_size
+				4,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -491,5 +502,6 @@
 		#undef NUM_CANNON_ROUNDS
 		#undef NUM_LAU69A_ROCKETS
 		#undef NUM_SIDEWINDER_MISSILES
+		#undef NUM_MAVERICK_MISSILES
 		#undef NUM_CHAFFS
 		#undef NUM_FLARES
