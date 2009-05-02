4e51152ddef34f4d92a5c5a023d7e914d63959a7
arneh <arneh>
2009-05-02 12:31:05 +0000

    Changed default loadout to be just rockets.

--- aphavoc/source/entity/mobile/weapon/configs/blckhawk.h
+++ aphavoc/source/entity/mobile/weapon/configs/blckhawk.h
@@ -1,62 +1,62 @@
-// 
+//
 // 	 Enemy Engaged RAH-66 Comanche Versus KA-52 Hokum
 // 	 Copyright (C) 2000 Empire Interactive (Europe) Ltd,
 // 	 677 High Road, North Finchley, London N12 0DA
-// 
+//
 // 	 Please see the document LICENSE.TXT for the full licence agreement
-// 
+//
 // 2. LICENCE
-//  2.1 	
-//  	Subject to the provisions of this Agreement we now grant to you the 
+//  2.1
+//  	Subject to the provisions of this Agreement we now grant to you the
 //  	following rights in respect of the Source Code:
-//   2.1.1 
-//   	the non-exclusive right to Exploit  the Source Code and Executable 
-//   	Code on any medium; and 
-//   2.1.2 
+//   2.1.1
+//   	the non-exclusive right to Exploit  the Source Code and Executable
+//   	Code on any medium; and
+//   2.1.2
 //   	the non-exclusive right to create and distribute Derivative Works.
-//  2.2 	
+//  2.2
 //  	Subject to the provisions of this Agreement we now grant you the
 // 	following rights in respect of the Object Code:
-//   2.2.1 
+//   2.2.1
 // 	the non-exclusive right to Exploit the Object Code on the same
 // 	terms and conditions set out in clause 3, provided that any
 // 	distribution is done so on the terms of this Agreement and is
 // 	accompanied by the Source Code and Executable Code (as
 // 	applicable).
-// 
+//
 // 3. GENERAL OBLIGATIONS
-//  3.1 
+//  3.1
 //  	In consideration of the licence granted in clause 2.1 you now agree:
-//   3.1.1 
+//   3.1.1
 // 	that when you distribute the Source Code or Executable Code or
 // 	any Derivative Works to Recipients you will also include the
 // 	terms of this Agreement;
-//   3.1.2 
+//   3.1.2
 // 	that when you make the Source Code, Executable Code or any
 // 	Derivative Works ("Materials") available to download, you will
 // 	ensure that Recipients must accept the terms of this Agreement
 // 	before being allowed to download such Materials;
-//   3.1.3 
+//   3.1.3
 // 	that by Exploiting the Source Code or Executable Code you may
 // 	not impose any further restrictions on a Recipient's subsequent
 // 	Exploitation of the Source Code or Executable Code other than
 // 	those contained in the terms and conditions of this Agreement;
-//   3.1.4 
+//   3.1.4
 // 	not (and not to allow any third party) to profit or make any
 // 	charge for the Source Code, or Executable Code, any
 // 	Exploitation of the Source Code or Executable Code, or for any
 // 	Derivative Works;
-//   3.1.5 
-// 	not to place any restrictions on the operability of the Source 
+//   3.1.5
+// 	not to place any restrictions on the operability of the Source
 // 	Code;
-//   3.1.6 
+//   3.1.6
 // 	to attach prominent notices to any Derivative Works stating
 // 	that you have changed the Source Code or Executable Code and to
 // 	include the details anddate of such change; and
-//   3.1.7 
+//   3.1.7
 //   	not to Exploit the Source Code or Executable Code otherwise than
 // 	as expressly permitted by  this Agreement.
-// 
+//
 
 
 
@@ -138,14 +138,14 @@
 			},
 			// WEAPON PACKAGE 3
 			{
-				ENTITY_SUB_TYPE_WEAPON_AGM114L_LONGBOW_HELLFIRE,	// sub_type
-				NUM_HELLFIRE_MISSILES,										// number
+				ENTITY_SUB_TYPE_WEAPON_HYDRA70_M261,					// sub_type
+				NUM_HYDRA_ROCKETS,											// number
 				LHS_HARDPOINT1,												// heading_depth
 				HELLFIRE_RACK,													// pitch_depth
 				0,					  												// muzzle_depth
 				FALSE,			  												// make_weapon_system_ready
 				FALSE,															// rotate
-				1,																	// salvo_size
+				8,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -160,14 +160,14 @@
 			},
 			// WEAPON PACKAGE 4
 			{
-				ENTITY_SUB_TYPE_WEAPON_AGM114L_LONGBOW_HELLFIRE,	// sub_type
-				NUM_HELLFIRE_MISSILES,										// number
+				ENTITY_SUB_TYPE_WEAPON_HYDRA70_M261,					// sub_type
+				NUM_HYDRA_ROCKETS,											// number
 				RHS_HARDPOINT1,												// heading_depth
 				HELLFIRE_RACK,													// pitch_depth
 				0,					  												// muzzle_depth
 				FALSE,			  												// make_weapon_system_ready
 				FALSE,															// rotate
-				1,																	// salvo_size
+				8,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -189,7 +189,7 @@
 				0,					  												// muzzle_depth
 				FALSE,			  												// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				8,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -211,7 +211,7 @@
 				0,					  												// muzzle_depth
 				FALSE,			  												// make_weapon_system_ready
 				FALSE,															// rotate
-				2,																	// salvo_size
+				8,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -277,7 +277,7 @@
 			// WEAPON PACKAGE 32
 			{ENTITY_SUB_TYPE_WEAPON_NO_WEAPON},
 		},
-////Moje030525 Added all WEAPON_CONFIG_TYPEs below 
+////Moje030525 Added all WEAPON_CONFIG_TYPEs below
 		////////////////////////////////////////
 		//
 		// WEAPON_CONFIG_TYPE_UH60_BLACK_HAWK_2
@@ -1358,16 +1358,16 @@
 				0,		 	 														// pitch_share_mask
 				MUZZLE_FLASH_INVALID,										// muzzle_flash_type
 			},
-			// WEAPON PACKAGE 5
+						// WEAPON PACKAGE 5
 			{
-				ENTITY_SUB_TYPE_WEAPON_HYDRA70_M261,					// sub_type
-				NUM_HYDRA_ROCKETS,											// number
+				ENTITY_SUB_TYPE_WEAPON_AGM114L_LONGBOW_HELLFIRE,	// sub_type
+				NUM_HELLFIRE_MISSILES,										// number
 				LHS_HARDPOINT2,												// heading_depth
-				HYDRA_POD,														// pitch_depth
+				HELLFIRE_RACK,													// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
-				TRUE,																// rotate
-				2,																	// salvo_size
+				FALSE,															// rotate
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -1376,20 +1376,20 @@
 				rad (0.0),														// max_pitch_limit
 				rad (0.0),														// muzzle_rotate_rate
 				knots_to_metres_per_second (10000.0),					// rotate_inhibit_velocity
-				0,		 															// heading_share_mask
-				0,		 	 														// pitch_share_mask
-				MUZZLE_FLASH_INVALID,									// muzzle_flash_type
+				0,																	// heading_share_mask
+				0,																	// pitch_share_mask
+				MUZZLE_FLASH_INVALID,										// muzzle_flash_type
 			},
 			// WEAPON PACKAGE 6
 			{
-				ENTITY_SUB_TYPE_WEAPON_HYDRA70_M261,					// sub_type
-				NUM_HYDRA_ROCKETS,											// number
+				ENTITY_SUB_TYPE_WEAPON_AGM114L_LONGBOW_HELLFIRE,	// sub_type
+				NUM_HELLFIRE_MISSILES,										// number
 				RHS_HARDPOINT2,												// heading_depth
-				HYDRA_POD,														// pitch_depth
+				HELLFIRE_RACK,													// pitch_depth
 				0,																	// muzzle_depth
 				FALSE,															// make_weapon_system_ready
-				TRUE,																// rotate
-				2,																	// salvo_size
+				FALSE,															// rotate
+				1,																	// salvo_size
 				rad (0.0),														// heading_rate
 				rad (0.0),														// min_heading_limit
 				rad (0.0),														// max_heading_limit
@@ -1398,9 +1398,9 @@
 				rad (0.0),														// max_pitch_limit
 				rad (0.0),														// muzzle_rotate_rate
 				knots_to_metres_per_second (10000.0),					// rotate_inhibit_velocity
-				0,		 															// heading_share_mask
-				0,		 	 														// pitch_share_mask
-				MUZZLE_FLASH_INVALID,								// muzzle_flash_type
+				0,																	// heading_share_mask
+				0,																	// pitch_share_mask
+				MUZZLE_FLASH_INVALID,										// muzzle_flash_type
 			},
 			// WEAPON PACKAGE 7
 			{ENTITY_SUB_TYPE_WEAPON_NO_WEAPON},
