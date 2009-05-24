25eee8c67125b3a897e0a7cb9a4150991a59cab6
arneh <arneh>
2009-05-24 12:45:06 +0000

    Add tacview logging for weapons.

--- aphavoc/source/entity/mobile/weapon/wn_creat.c
+++ aphavoc/source/entity/mobile/weapon/wn_creat.c
@@ -1,62 +1,62 @@
-// 
+//
 // 	 Enemy Engaged RAH-66 Comanche Versus KA-52 Hokum
 // 	 Copyright (C) 2000 Empire Interactive (Europe) Ltd,
 // 	 677 High Road, North Finchley, London N12 0DA
-// 
+//
 // 	 Please see the document LICENSE.TXT for the full licence agreement
-// 
+//
 // 2. LICENCE
-//  2.1 	
-//  	Subject to the provisions of this Agreement we now grant to you the 
+//  2.1
+//  	Subject to the provisions of this Agreement we now grant to you the
 //  	following rights in respect of the Source Code:
-//   2.1.1 
-//   	the non-exclusive right to Exploit  the Source Code and Executable 
-//   	Code on any medium; and 
-//   2.1.2 
+//   2.1.1
+//   	the non-exclusive right to Exploit  the Source Code and Executable
+//   	Code on any medium; and
+//   2.1.2
 //   	the non-exclusive right to create and distribute Derivative Works.
-//  2.2 	
+//  2.2
 //  	Subject to the provisions of this Agreement we now grant you the
 // 	following rights in respect of the Object Code:
-//   2.2.1 
+//   2.2.1
 // 	the non-exclusive right to Exploit the Object Code on the same
 // 	terms and conditions set out in clause 3, provided that any
 // 	distribution is done so on the terms of this Agreement and is
 // 	accompanied by the Source Code and Executable Code (as
 // 	applicable).
-// 
+//
 // 3. GENERAL OBLIGATIONS
-//  3.1 
+//  3.1
 //  	In consideration of the licence granted in clause 2.1 you now agree:
-//   3.1.1 
+//   3.1.1
 // 	that when you distribute the Source Code or Executable Code or
 // 	any Derivative Works to Recipients you will also include the
 // 	terms of this Agreement;
-//   3.1.2 
+//   3.1.2
 // 	that when you make the Source Code, Executable Code or any
 // 	Derivative Works ("Materials") available to download, you will
 // 	ensure that Recipients must accept the terms of this Agreement
 // 	before being allowed to download such Materials;
-//   3.1.3 
+//   3.1.3
 // 	that by Exploiting the Source Code or Executable Code you may
 // 	not impose any further restrictions on a Recipient's subsequent
 // 	Exploitation of the Source Code or Executable Code other than
 // 	those contained in the terms and conditions of this Agreement;
-//   3.1.4 
+//   3.1.4
 // 	not (and not to allow any third party) to profit or make any
 // 	charge for the Source Code, or Executable Code, any
 // 	Exploitation of the Source Code or Executable Code, or for any
 // 	Derivative Works;
-//   3.1.5 
-// 	not to place any restrictions on the operability of the Source 
+//   3.1.5
+// 	not to place any restrictions on the operability of the Source
 // 	Code;
-//   3.1.6 
+//   3.1.6
 // 	to attach prominent notices to any Derivative Works stating
 // 	that you have changed the Source Code or Executable Code and to
 // 	include the details anddate of such change; and
-//   3.1.7 
+//   3.1.7
 //   	not to Exploit the Source Code or Executable Code otherwise than
 // 	as expressly permitted by  this Agreement.
-// 
+//
 
 
 
@@ -65,6 +65,8 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "project.h"
+
+#include "entity/tacview/tacview.h"
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -109,7 +111,7 @@ static entity *create_local (entity_types type, int index, char *pargs)
 	if (en)
 	{
 		float dispersion;
-		
+
 		////////////////////////////////////////
    	//
    	// MALLOC ENTITY DATA
@@ -250,6 +252,9 @@ static entity *create_local (entity_types type, int index, char *pargs)
 		insert_local_entity_into_parents_child_list (en, LIST_TYPE_SECTOR, get_local_sector_entity (&raw->mob.position), NULL);
 
 		insert_local_entity_into_parents_child_list (en, LIST_TYPE_UPDATE, get_update_entity (), NULL);
+
+		if (tacview_is_logging())
+			write_tacview_new_unit(en);
 
 		//
 		// check if the weapon camera is primed and this weapon has been launched by the external view entity
@@ -424,7 +429,7 @@ void create_client_server_entity_weapon (entity *launcher, entity_sub_types weap
 		if (current_weapon_count > 0)
 		{
 			int loal_mode = weapon_database[weapon_sub_type].hellfire_flight_profile && get_local_entity_int_value (launcher, INT_TYPE_LOCK_ON_AFTER_LAUNCH);
-			
+
 			if (get_comms_data_flow () == COMMS_DATA_FLOW_RX)
 			{
 				set_force_local_entity_create_stack_attributes (TRUE);
@@ -444,23 +449,23 @@ void create_client_server_entity_weapon (entity *launcher, entity_sub_types weap
 				case ENTITY_TYPE_HELICOPTER:
 					last_shot = &((helicopter*)get_local_entity_data(launcher))->ac.weapon_salvo_timer;
 					break;
-					
+
 				case ENTITY_TYPE_FIXED_WING:
 					last_shot = &((fixed_wing*)get_local_entity_data(launcher))->ac.weapon_salvo_timer;
 					break;
-				
+
 				case ENTITY_TYPE_ANTI_AIRCRAFT:
 					last_shot = &((anti_aircraft*)get_local_entity_data(launcher))->vh.weapon_salvo_timer;
 					break;
-				
+
 				case ENTITY_TYPE_PERSON:
 					last_shot = &((person*)get_local_entity_data(launcher))->vh.weapon_salvo_timer;
 					break;
-				
+
 				case ENTITY_TYPE_ROUTED_VEHICLE:
 					last_shot = &((routed_vehicle*)get_local_entity_data(launcher))->vh.weapon_salvo_timer;
 					break;
-				
+
 				case ENTITY_TYPE_SHIP_VEHICLE:
 					last_shot = &((ship_vehicle*)get_local_entity_data(launcher))->vh.weapon_salvo_timer;
 					break;
