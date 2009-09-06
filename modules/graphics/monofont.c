52fe80bb322144deeb2ea9ac89f252c3b3fd2cb7
arneh <arneh>
2009-09-06 15:46:12 +0000

    Added new 12x20 font.

--- modules/graphics/monofont.c
+++ modules/graphics/monofont.c
@@ -1,62 +1,62 @@
-// 
+//
 // 	 Enemy Engaged RAH-66 Comanche Versus KA-52 Hokum
 // 	 Copyright (C) 2000 Empire Interactive (Europe) Ltd,
 // 	 677 High Road, North Finchley, London N12 0DA
-// 
+//
 // 	 Please see the document LICENSE.TXT for the full licence agreement
-// 
+//
 // 2. LICENCE
-//  2.1 	
-//  	Subject to the provisions of this Agreement we now grant to you the 
+//  2.1
+//  	Subject to the provisions of this Agreement we now grant to you the
 //  	following rights in respect of the Source Code:
-//   2.1.1 
-//   	the non-exclusive right to Exploit  the Source Code and Executable 
-//   	Code on any medium; and 
-//   2.1.2 
+//   2.1.1
+//   	the non-exclusive right to Exploit  the Source Code and Executable
+//   	Code on any medium; and
+//   2.1.2
 //   	the non-exclusive right to create and distribute Derivative Works.
-//  2.2 	
+//  2.2
 //  	Subject to the provisions of this Agreement we now grant you the
 // 	following rights in respect of the Object Code:
-//   2.2.1 
+//   2.2.1
 // 	the non-exclusive right to Exploit the Object Code on the same
 // 	terms and conditions set out in clause 3, provided that any
 // 	distribution is done so on the terms of this Agreement and is
 // 	accompanied by the Source Code and Executable Code (as
 // 	applicable).
-// 
+//
 // 3. GENERAL OBLIGATIONS
-//  3.1 
+//  3.1
 //  	In consideration of the licence granted in clause 2.1 you now agree:
-//   3.1.1 
+//   3.1.1
 // 	that when you distribute the Source Code or Executable Code or
 // 	any Derivative Works to Recipients you will also include the
 // 	terms of this Agreement;
-//   3.1.2 
+//   3.1.2
 // 	that when you make the Source Code, Executable Code or any
 // 	Derivative Works ("Materials") available to download, you will
 // 	ensure that Recipients must accept the terms of this Agreement
 // 	before being allowed to download such Materials;
-//   3.1.3 
+//   3.1.3
 // 	that by Exploiting the Source Code or Executable Code you may
 // 	not impose any further restrictions on a Recipient's subsequent
 // 	Exploitation of the Source Code or Executable Code other than
 // 	those contained in the terms and conditions of this Agreement;
-//   3.1.4 
+//   3.1.4
 // 	not (and not to allow any third party) to profit or make any
 // 	charge for the Source Code, or Executable Code, any
 // 	Exploitation of the Source Code or Executable Code, or for any
 // 	Derivative Works;
-//   3.1.5 
-// 	not to place any restrictions on the operability of the Source 
+//   3.1.5
+// 	not to place any restrictions on the operability of the Source
 // 	Code;
-//   3.1.6 
+//   3.1.6
 // 	to attach prominent notices to any Derivative Works stating
 // 	that you have changed the Source Code or Executable Code and to
 // 	include the details anddate of such change; and
-//   3.1.7 
+//   3.1.7
 //   	not to Exploit the Source Code or Executable Code otherwise than
 // 	as expressly permitted by  this Agreement.
-// 
+//
 
 
 
@@ -178,6 +178,16 @@ static const char
 		#include "mono10x16.h"
 	};
 
+#define CELL_WIDTH_12X20	(12)
+#define CELL_HEIGHT_12X20	(20)
+#define CELL_SIZE_12X20		(CELL_HEADER_SIZE + CELL_WIDTH_12X20 * CELL_HEIGHT_12X20)
+
+static const char
+	mono_font_12x20_cells[NUM_CHARACTERS][CELL_SIZE_12X20] =
+	{
+		#include "mono12x20.h"
+	};
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -192,7 +202,8 @@ static const char
 		( const char * ) mono_font_6x10_cells,
 		( const char * ) mono_font_7x12_cells,
 		( const char * ) mono_font_8x14_cells,
-		( const char * ) mono_font_10x16_cells
+		( const char * ) mono_font_10x16_cells,
+		( const char * ) mono_font_12x20_cells
 	};
 
 static const float
@@ -206,6 +217,7 @@ static const float
 		CELL_WIDTH_7X12,
 		CELL_WIDTH_8X14,
 		CELL_WIDTH_10X16,
+		CELL_WIDTH_12X20,
 	};
 
 static const float
@@ -219,6 +231,7 @@ static const float
 		CELL_HEIGHT_7X12,
 		CELL_HEIGHT_8X14,
 		CELL_HEIGHT_10X16,
+		CELL_HEIGHT_12X20,
 	};
 
 static const int
@@ -232,6 +245,7 @@ static const int
 		CELL_SIZE_7X12,
 		CELL_SIZE_8X14,
 		CELL_SIZE_10X16,
+		CELL_SIZE_12X20,
 	};
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
