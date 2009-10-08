1c97c04abfd7925564c54fac15804e1ebd329f4a
arneh <arneh>
2009-10-08 22:52:26 +0000

    Save a little on (de)allocations by allowing an existing object to be used.

--- modules/system/ev_stack.c
+++ modules/system/ev_stack.c
@@ -1,62 +1,62 @@
-// 
+//
 // 	 Enemy Engaged RAH-66 Comanche Versus KA-52 Hokum
 // 	 Copyright (C) 2000 Empire Interactive (Europe) Ltd,
 // 	 677 High Road, North Finchley, London N12 0DA
-// 
+//
 // 	 Please see the document LICENSE.TXT for the full licence agreement
-// 
+//
 // 2. LICENCE
-//  2.1 	
-//  	Subject to the provisions of this Agreement we now grant to you the 
+//  2.1
+//  	Subject to the provisions of this Agreement we now grant to you the
 //  	following rights in respect of the Source Code:
-//   2.1.1 
-//   	the non-exclusive right to Exploit  the Source Code and Executable 
-//   	Code on any medium; and 
-//   2.1.2 
+//   2.1.1
+//   	the non-exclusive right to Exploit  the Source Code and Executable
+//   	Code on any medium; and
+//   2.1.2
 //   	the non-exclusive right to create and distribute Derivative Works.
-//  2.2 	
+//  2.2
 //  	Subject to the provisions of this Agreement we now grant you the
 // 	following rights in respect of the Object Code:
-//   2.2.1 
+//   2.2.1
 // 	the non-exclusive right to Exploit the Object Code on the same
 // 	terms and conditions set out in clause 3, provided that any
 // 	distribution is done so on the terms of this Agreement and is
 // 	accompanied by the Source Code and Executable Code (as
 // 	applicable).
-// 
+//
 // 3. GENERAL OBLIGATIONS
-//  3.1 
+//  3.1
 //  	In consideration of the licence granted in clause 2.1 you now agree:
-//   3.1.1 
+//   3.1.1
 // 	that when you distribute the Source Code or Executable Code or
 // 	any Derivative Works to Recipients you will also include the
 // 	terms of this Agreement;
-//   3.1.2 
+//   3.1.2
 // 	that when you make the Source Code, Executable Code or any
 // 	Derivative Works ("Materials") available to download, you will
 // 	ensure that Recipients must accept the terms of this Agreement
 // 	before being allowed to download such Materials;
-//   3.1.3 
+//   3.1.3
 // 	that by Exploiting the Source Code or Executable Code you may
 // 	not impose any further restrictions on a Recipient's subsequent
 // 	Exploitation of the Source Code or Executable Code other than
 // 	those contained in the terms and conditions of this Agreement;
-//   3.1.4 
+//   3.1.4
 // 	not (and not to allow any third party) to profit or make any
 // 	charge for the Source Code, or Executable Code, any
 // 	Exploitation of the Source Code or Executable Code, or for any
 // 	Derivative Works;
-//   3.1.5 
-// 	not to place any restrictions on the operability of the Source 
+//   3.1.5
+// 	not to place any restrictions on the operability of the Source
 // 	Code;
-//   3.1.6 
+//   3.1.6
 // 	to attach prominent notices to any Derivative Works stating
 // 	that you have changed the Source Code or Executable Code and to
 // 	include the details anddate of such change; and
-//   3.1.7 
+//   3.1.7
 //   	not to Exploit the Source Code or Executable Code otherwise than
 // 	as expressly permitted by  this Agreement.
-// 
+//
 
 
 
@@ -82,7 +82,7 @@ event_stack
 
 static int
 	event_id;
-	
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -96,7 +96,7 @@ static void display_event_stack (void);
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
+
 void initialise_event_stack (void)
 {
 
@@ -110,7 +110,7 @@ void initialise_event_stack (void)
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
+
 void deinitialise_event_stack (void)
 {
 
@@ -245,9 +245,9 @@ void push_event (void ((*this_function) (void)), const char *name)
 
 	if (name)
 	{
-	
+
 		new_stack_item->name = (char *) safe_malloc (sizeof (char) * (strlen (name) + 1));
-	
+
 		sprintf (new_stack_item->name, "%s", name);
 	}
 
@@ -295,13 +295,20 @@ void push_event (void ((*this_function) (void)), const char *name)
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // push events, without clearing event table first. Previous events will 'show through' the current event layer
-
 void push_event_overlay (void ((*this_function) (void)), const char *name)
+{
+	// malloc new event
+
+	event_stack* stack_item = (event_stack *) safe_malloc (sizeof (event_stack));
+
+	push_event_overlay_with_stack_item(this_function, name, stack_item);
+}
+
+void push_event_overlay_with_stack_item(void ((*this_function) (void)), const char *name, event_stack* new_stack_item)
 {
 
 	event_stack
-		*stack_search,
-		*new_stack_item;
+		*stack_search;
 
 	// check if already on stack
 
@@ -375,10 +382,6 @@ void push_event_overlay (void ((*this_function) (void)), const char *name)
 
 	#endif
 
-	// malloc new event
-
-	new_stack_item = (event_stack *) safe_malloc (sizeof (event_stack));
-
 	new_stack_item->overlay = TRUE;
 
 	new_stack_item->function = this_function;
@@ -389,9 +392,9 @@ void push_event_overlay (void ((*this_function) (void)), const char *name)
 
 	if (name)
 	{
-	
+
 		new_stack_item->name = (char *) safe_malloc (sizeof (char) * (strlen (name) + 1));
-	
+
 		sprintf (new_stack_item->name, "%s", name);
 	}
 
@@ -503,7 +506,12 @@ void push_event_stop (void)
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void pop_event (void ((*function) (void)))
+void pop_event(void ((*function) (void)))
+{
+	pop_event_delete(function, TRUE);
+}
+
+void pop_event_delete(void ((*function) (void)), int delete_stack_item)
 {
 
 	event_stack
@@ -521,7 +529,7 @@ void pop_event (void ((*function) (void)))
 
 	if (event_stack_head)
 	{
-	
+
 		popped_event = event_stack_head;
 
 		//
@@ -558,14 +566,14 @@ void pop_event (void ((*function) (void)))
 		//
 		//
 		//
-	
+
 		event_stack_head = event_stack_head->prev;
-	
+
 		if (event_stack_head)
 		{
 
 			event_stack_head->next = NULL;
-	
+
   			// overlay with this event table
 
 			clear_events ();
@@ -619,8 +627,9 @@ void pop_event (void ((*function) (void)))
 
 			safe_free (popped_event->name);
 		}
-	
-		safe_free (popped_event);
+
+		if (delete_stack_item)
+			safe_free (popped_event);
 	}
 }
 
@@ -646,12 +655,12 @@ void pop_to_event_stop (void)
 		debug_log ("EV_STACK: popping to event stop ");
 
 		#endif
-	
+
   		while ((event_stack_head) && (event_stack_head->function))
 		{
-	
+
 			popped_event = event_stack_head;
-	
+
 			event_stack_head = event_stack_head->prev;
 
 			#if DEBUG_MODULE
@@ -671,10 +680,10 @@ void pop_to_event_stop (void)
 
 			if (popped_event->name)
 			{
-	
+
 				safe_free (popped_event->name);
 			}
-		
+
 			safe_free (popped_event);
 		}
 
@@ -712,18 +721,18 @@ void display_event_stack (void)
 		debug_log ("EV_STACK: -------------------------------------------------------------------------------");
 
 		debug_log ("EV_STACK: current event stack ");
-	
+
   		while (ev)
 		{
 
 			if (ev->name)
 			{
-	
+
 				debug_log ("EV_STACK:     %s %d, function %d, overlay %d", ev->name, ev, ev->function, ev->overlay);
 			}
 			else
 			{
-	
+
 				debug_log ("EV_STACK:     %d, function %d, overlay %d", ev, ev->function, ev->overlay);
 			}
 
