025e705eedd3519f01f00fe51407acfa8a6e70f1
unknown <arneh@.(none)>
2010-04-12 20:01:17 +0200

    Add compile time to cmake script

--- aphavoc/cmake.bat
+++ aphavoc/cmake.bat
@@ -1,4 +1,15 @@
 @echo off
 SET EECH_ROOTDIR=c:\eech-dev
 del project.exe
+
+echo ################### Make Timings ####################### >> maketime.txt
+echo. | time < dmake.bat >> maketime.txt
+
 wmake optimise=1 commercial=1
+
+echo. | time < dmake.bat >> maketime.txt
+echo ######################################################## >> maketime.txt
+   
+type maketime.txt 
+
+del maketime.txt
