bb0f5f617f0c58e7545c8d6e0ad76be3991a8ae4
Tony <tnmcelroy@gmail.com>
2017-10-26 01:08:20 +0100

    Adding bat files to repo

---
+++ aphavoc/cmake.bat
@@ -0,0 +1,4 @@
+@echo off
+SET EECH_ROOTDIR=add code directory
+del project.exe
+wmake optimise=1 commercial=1
---
+++ aphavoc/vmake.bat
@@ -0,0 +1,4 @@
+@echo off
+SET EECH_ROOTDIR=add code directory
+
+   wmake debug=1 optimise=1 visualc=1
